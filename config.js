3c2cf5b8c1cbb3a04bcd2c4e2bd968d820167ce5
alexazf <dmitriyev.alexey@gmail.com>
2020-03-25 03:35:11 +0600

    feat: role manager bot

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: 'Discord Bot token',
+    botId: 'bot id',
+
+    channelIds: [],
+    maxPrefixLength: null 
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require('./config');
+const re = /\/tag (\w+)/i;
+const nicknameRegex = /(\[\w+\]) (.+)/i;
+
+client.once('ready', () => console.log('Bot started!'));
+
+const setRole = async (message, data) => {
+    console.log(message.guild.roles.cache);
+    let role = message.guild.roles.cache.find(role => role.name === data.name);
+
+    if (!role) {
+        role = await message.guild.roles.create({
+            data
+        });
+    }
+
+    try {
+        await message.member.roles.set([role.id]);
+    } catch (err) {
+        message.channel.send(`Ошибка при выдачи роли: ${err.message}`);
+    }
+}
+
+client.on('message', async (message) => {
+    if (message.author.id === config.botId) return;
+    if (!config.channelIds.includes(message.channel.id) && config.channelIds.length !== 0) return;
+
+    if (message.content === 'ping') message.channel.send(`pong`);
+    if (/\/id/i.test(message.content)) message.channel.send(`ID этого канала - ${message.channel.id}`);
+
+    if (re.test(message.content)) {
+        const [text, prefix] = message.content.match(re);
+        let oldNickname = message.member.displayName;
+
+        if (prefix.length > config.maxPrefixLength) {
+            message.channel.send(`Ошибка: заданное название команды больше допустимого лимита (>${config.maxPrefixLength})`);
+            return;
+        }
+
+        try {
+            if (nicknameRegex.test(oldNickname)) {
+                oldNickname = oldNickname.match(nicknameRegex)[2];
+            }
+
+            await message.member.setNickname(`[${prefix}] ${oldNickname}`);
+            await setRole(message, {
+                name: prefix,
+                hoist: true,
+                color: 'RANDOM'
+            });
+            message.react('👌🏻');
+
+            message.delete({ timeout: 5000 });
+        } catch (err) {
+            message.channel.send(`Ошибка при смене ника: ${err.message}`);
+        }
+    }
+});
+
+client.login(config.token);
